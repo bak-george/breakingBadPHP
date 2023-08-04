64d2b7841aa6ecd3c2ecf46ddb9c1b0b02946470
George Bakoulis <g.bakoulis@outlook.com>
2023-08-04 11:46:21 +0000

    metadata update

--- views/partials/head.php
+++ views/partials/head.php
@@ -4,16 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Breaking Bad Quote Generator 
-    contains quotes from the universe of Breaking Bad and Better Call Saul. 
-    Most of the quotes are from Walter White (Heisenberg), Jessy Pinkman,
-    Skyler White, Saul Goodman and Mike Ehrmantraut. Developed by George Bakoulis">
-    <link rel="icon" type="image/x-icon" href="images/walterwhite-breakingBadQuoteGenerator.svg"> 
+    <meta name="title" content="Most Popular Quotes from Breaking Bad">
+    <meta name="description" content="Find quotes from the universe of Breaking Bad on the Breaking Bad quote generator. Most of the quotes are from Walter White, Jessy Pinkman, and Saul Goodman.">
+    <link rel="icon" type="image/x-icon" href="images/walterwhite-breakingBadQuoteGenerator.svg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet"  href="../app.css">
     <link rel="stylesheet" href="../mobile.css">
    <link rel="stylesheet" href="../loader.css">
-    <?php 
+    <?php
       if ($heading === 'about') { ?>
           <link rel="stylesheet" href="../about.css">
       <?php } ?>
@@ -21,14 +19,13 @@
         window.onload = function() {
       setTimeout(function() {
         var elements = document.getElementsByClassName("loader");
-        
         for (var i = 0; i < elements.length; i++) {
           elements[i].style.display = "none"; 
         }
       }, 1000);
     }
   </script>
-    <title>Breaking Bad Quotes</title>
+    <title>Breaking Bad Quotes Generator</title>
 </head>
 <body>
     <div class="loader">
