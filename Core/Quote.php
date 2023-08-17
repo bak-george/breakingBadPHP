08e9b1084de4ecbc00d3eb561b17585bfdfb0011
Bakoulis George <g.bakoulis@outlook.com>
2023-08-17 21:22:58 +0300

    fixed the issue with the recurring quotes

--- Core/Quote.php
+++ Core/Quote.php
@@ -6,15 +6,43 @@ class Quote
 {
   public static function getRandomQuote($db)
   {
-    $sqlQuote = "SELECT * FROM quotes ORDER BY RAND() LIMIT 1";
+    if (session_status() == PHP_SESSION_NONE) {
+      session_start();
+    }
+
+    if (!isset($_SESSION['seen_quotes'])) {
+      $_SESSION['seen_quotes'] = [];
+      array_unshift($_SESSION['seen_quotes'], 1);
+    }
+
+    $totalQuotes = $db->query("SELECT COUNT(*) as total FROM quotes")->get();
+    echo "seen quote count:\n";
+    var_dump($_SESSION['seen_quotes']);
+    if(count($_SESSION['seen_quotes']) >= $totalQuotes) {
+      
+    }
+
+    $seenQuotesCondition = "";
+
+    if (!empty($_SESSION['seen_quotes'])) {
+      $seenQuotes = implode(',', $_SESSION['seen_quotes']);
+      $seenQuotesCondition = "WHERE id NOT IN ($seenQuotes)";
+    }
+    
+    $sqlQuote = "SELECT * FROM quotes {$seenQuotesCondition} ORDER BY RAND() LIMIT 1";
   
     $randomQuote = $db->query($sqlQuote)->get();
  
     if ($randomQuote) {
+      array_unshift($_SESSION['seen_quotes'], $randomQuote[0]['id']);
+
       return $randomQuote;
+    } else {
+      $_SESSION['seen_quotes'] = [];
+      header('location: /quote');
+      exit();
     }
   
     return false;
   }
 }
-
--- config.php
+++ config.php
@@ -2,13 +2,13 @@
 
 return [
   'database' => [
-    'host' => 'db-mysql-fra1-71124-do-user-12961304-0.b.db.ondigitalocean.com',
-    'port' => 25060,
-    'dbname' => 'breaking_bad'
+    'host' => '127.0.0.1',  // This is the standard IP address for localhost
+    'port' => 3306,         // This is the default MySQL port. Adjust if your local setup is different.
+    'dbname' => 'breaking_bad_local'  // Keep the database name same if you have the same local database name.
   ],
-  'username' => 'doadmin',
-  'password' => 'AVNS_oeI5IduHGw2qvKVj66p',
+  'username' => 'root',  // This is a common local MySQL username. Adjust according to your local setup.
+  'password' => '',      // Typically, local MySQL setups might not have a password. If yours does, replace the empty string with it.
   'recaptcha' => [
-    'secretKey' => '6LdZgEUnAAAAAE4OTdtz8eWJogSuAK1afpKbfVTW'
+    'secretKey' => '6LdZgEUnAAAAAE4OTdtz8eWJogSuAK1afpKbfVTW' // If you're just testing locally, you may not need to change this. If you have a different key for local development, replace it here.
   ]
 ];
